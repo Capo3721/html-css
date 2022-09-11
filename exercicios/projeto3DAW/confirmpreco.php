0887f7bee21667402900bfb24cdbbf78a7e593be
Arthur Prado <83094040+Capo3721@users.noreply.github.com>
2022-09-11 20:37:18 -0300

    Att projeto 3DAW
    Atualização

---
+++ exercicios/projeto3DAW/confirmpreco.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirme o Preço</title>
+    <link rel="stylesheet" href="cad.css">
+</head>
+<body>
+    <header>
+        Serviço Fácil
+
+    </header>
+
+    <section class="container">
+    
+        <form class="valores" method="POST">
+            <p>Confirme o preço do seu serviço</p>
+            <label for="tp_servico">Tipo de serviço: </label>
+            <input style="width: 140px;" type="text"><br><br>
+            <label for="servico">Serviço: </label>
+            <input style="width: 205px;" type="text"><br><br>
+            <label for="preco">Preço: </label>
+            <input style="width: 220px;" type="text"><br><br>
+            <label for="credito">Seu crédito: </label>
+            <input style="width: 165px;" type="text"><br><br>
+            <label for="val_cobrado">Valor a ser cobrado: </label>
+            <input style="width: 50px;" type="text"><br><br>
+
+            <input class="c-button" type="button" value="Confirmar">
+            
+        </form>                                               
+    </section>
+
+
+    <footer>
+        Site criado por: Arthur Prado, João e Victor Santos
+    </footer>
+</body>
+</html>
---
+++ exercicios/projeto3DAW/solservic.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Solicitar Serviço</title>
+    <link rel="stylesheet" href="cad.css">
+</head>
+<body>
+    <header>
+        Serviço Fácil
+
+    </header>
+
+    <section class="container">
+    
+        <form class="login-form" method="POST">
+            <p>Escolha o serviço desejado: </p>
+            <label for="Email">Tipo de Serviço: </label>
+                <select class=interessados>
+                <option value="" >Serviços</option>
+                <option value="" >Mecânico</option>
+                <option value="" >Eletricista</option>
+                <option value="" >Bombeiro</option>
+            </select>
+            <article class="solopcao">
+                <p class="opcao">Tipo de serviço </p>
+                <p class="opcao">Tipo de serviço </p>
+                <p class="opcao">Tipo de serviço </p>
+            </article>
+
+          
+            <input class="c-button" type="button" value="Registrar">
+            
+        </form>                                               
+    </section>
+
+
+    <footer>
+        Site criado por: Arthur Prado, João e Victor Santos
+    </footer>
+</body>
+</html>
