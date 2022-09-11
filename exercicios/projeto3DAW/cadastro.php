4cd8b1137e469abac952a7bbb2cf3724e8ed296f
Arthur Prado <83094040+Capo3721@users.noreply.github.com>
2022-09-11 13:58:42 -0300

    Porjeto 3DAW

---
+++ exercicios/projeto3DAW/cadastro.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Cadastro</title>
+    <link rel="stylesheet" href="cad.css">
+</head>
+<body>
+    <header>
+        Serviço Fácil
+
+    </header>
+
+    <section class="container">
+    
+        <form class="login-form" method="POST">
+            <p><a href="#">< Cadastro de Cliente</a> | <a href="#">Prestador ></a></p>
+            <label for="Email">Email: </label>
+            <input style="width: 240px;" class="espace" type="text"><br><br>
+            <label for="Email">Senha: </label>
+            <input class="espace" type="text"><br><br>
+            <label for="Email">Redigite: </label>
+            <input style="width: 210px;" class="espace" type="text"><br><br>
+            <input class="c-button" type="button" value="Registrar">
+            
+        </form>                                               
+    </section>
+
+
+    <footer>
+        Site criado por: Arthur Prado, João e Victor Santos
+    </footer>
+</body>
+</html>
